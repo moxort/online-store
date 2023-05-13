f5e363d10cdac659f4fdf9af2c0c7f411144520d
Vladyslav Petlovyi <>
2023-05-13 12:59:56 +0300

    structure changes, views added

--- src/app/fake-data.ts
+++ src/app/fake-data.ts
@@ -5,66 +5,77 @@ export const fakeListings: Listing [] = [
     id: '123',
     name: 'Old Boat',
     description: 'A very old boat. Bargain price',
-    price: 700
+    price: 700,
+    views:0
   },
   {
     id: '789',
     name: 'Vintage Camera',
     description: 'A classic film camera. Fully functional.',
     price: 200,
+    views:0
   },
   {
     id: '234',
     name: 'Retro Phone',
     description: 'An old-school phone with rotary dial. Perfect for nostalgia lovers.',
     price: 50,
+    views:0
   },
   {
     id: '567',
     name: 'Antique Pocket Watch',
     description: 'A rare pocket watch from the early 1900s. Keeps perfect time.',
     price: 800,
+    views:0
   },
   {
     id: '890',
     name: 'Vintage Typewriter',
     description: 'A manual typewriter in excellent condition. Great for writers.',
     price: 300,
+    views:0
   },
   {
     id: '1234',
     name: 'Classic Car',
     description: 'A beautifully restored vintage car. Runs like a dream.',
     price: 15000,
+    views:0
   },
   {
     id: '5678',
     name: 'Gramophone',
     description: 'A vintage gramophone with a warm, rich sound. Comes with original records.',
     price: 600,
+    views:0
   },
   {
     id: '9012',
     name: 'Antique Desk',
     description: 'A stunning antique desk with intricate carvings. Perfect for a home office.',
     price: 1200,
+    views:0
   },
   {
     id: '3456',
     name: 'Retro Fridge',
     description: 'A vintage fridge with modern features.',
     price: 800,
+    views:0
   },
   {
     id: '7890',
     name: 'Classic Motorcycle',
     description: 'A rare and powerful motorcycle from the 1970s.',
     price: 5000,
+    views:0
   },
   {
     id: '12345',
     name: 'Antique Clock',
     description: 'An ornate grandfather clock with a chime that echoes through the house.',
     price: 2500,
+    views:0
   }
   ]
